c16f6e99ee3d3585aae6c327edbae31b3b9f191e
Tigh <tigtigertig@gmail.com>
2017-11-14 22:29:19 +0000

    Updated weather icons to include night

--- app/src/main/java/com/firstadie/csftcarroll/b00641329/firstaide/api/Weather.java
+++ app/src/main/java/com/firstadie/csftcarroll/b00641329/firstaide/api/Weather.java
@@ -46,42 +46,43 @@ public class Weather {
     public int getWeatherIcon() {
         switch (mWeatherCode) {
             case "01d":
-                return R.drawable.art_clear;
+                return R.drawable.weather_clear_day;
             case "01n":
-                return R.drawable.art_clear;
+                return R.drawable.weather_clear_night;
             case "02d":
-                return R.drawable.art_light_clouds;
+                return R.drawable.weather_light_clouds_day;
             case "02n":
-                return R.drawable.art_light_clouds;
+                return R.drawable.weather_light_clouds_night;
             case "03d":
-                return R.drawable.art_clouds;
+                return R.drawable.weather_clouds_day;
             case "03n":
-                return R.drawable.art_clouds;
+                return R.drawable.weather_clouds_night;
             case "04d":
-                return R.drawable.art_light_clouds;
+                return R.drawable.weather_light_clouds_day;
             case "04n":
-                return R.drawable.art_light_clouds;
+                return R.drawable.weather_light_clouds_night;
             case "09d":
-                return R.drawable.art_rain;
+                return R.drawable.weather_rain_day;
             case "09n":
-                return R.drawable.art_rain;
+                return R.drawable.weather_rain_night;
             case "10d":
-                return R.drawable.art_light_rain;
+                return R.drawable.weather_light_rain_day;
             case "10n":
-                return R.drawable.art_light_rain;
+                return R.drawable.weather_light_rain_night;
             case "11d":
-                return R.drawable.art_storm;
+                return R.drawable.weather_storm_day;
             case "11n":
-                return R.drawable.art_storm;
+                return R.drawable.weather_storm_night;
             case "13d":
-                return R.drawable.art_snow;
+                return R.drawable.weather_snow_day;
             case "13n":
-                return R.drawable.art_snow;
+                return R.drawable.weather_snow_night;
             case "50d":
-                return R.drawable.art_fog;
+                return R.drawable.weather_fog;
             case "50n":
-                return R.drawable.art_fog;
-            default: return R.drawable.art_clear;
+                return R.drawable.weather_fog;
+            default:
+                return R.drawable.weather_none_available;
 
         }
     }
